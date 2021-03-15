2595f66ad5b2907183120f718560207c5c2dc600
Your Name <kurskovp@gmail.com>
2021-03-16 02:13:21 +0300

    При создании нового SiderMan получаем: name Peter Parker - введено power 80 - введено energy, hands - взяты из родительского класса Character т.к. он указан первым. функция стрельбы берется из второго родительского класса

---
+++ lecture.py
@@ -0,0 +1,29 @@
+
+#и сразу реализуем множественное наследрвание!
+
+class Character():
+    name = ''
+    power =0
+    energy = 100
+    hands = 2
+
+class Spider():
+    power = 0
+    energy = 50
+    hands = 8
+
+    def webshoot(self):
+        print('Pew-Pew!')
+
+class SpiderMan(Character, Spider):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+
+peter_parker = SpiderMan('Peter Parker', 80)
+print(peter_parker.name)
+print(peter_parker.power)
+print(peter_parker.energy)
+print(peter_parker.hands)
+peter_parker.webshoot()
