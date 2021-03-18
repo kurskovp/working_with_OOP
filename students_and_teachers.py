8820fc9c3a2b46d2570720d8bfe652fd0c8686d4
Your Name <kurskovp@gmail.com>
2021-03-18 15:16:56 +0300

    Задание №2 - Добавлено в классы Student и Reviewer возможность выставлять оценки за ведение лекций и за выполненные ДЗ

--- students_and_teachers.py
+++ students_and_teachers.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = []
 
+    def set_lecturer_dradegra(self, lecturer, course, grade):
+        if course in self.courses_in_progress:
+            if course in lecturer.teaching_course:
+                lecturer.grades[course] = grade
+            else:
+                print(f'Преподаватель этот курс не ведёт - {course}')
+        else:
+            print(f'Студент не записан на данный курс, оценку лектору поставить не'
+                  f'может - {course}')
+
 
 class Mentor:
     def __init__(self, name, surname, teaching_course):
@@ -23,4 +33,13 @@ class Lecturer(Mentor):
 class Reviewer(Mentor):
     def __init__(self, name, surname, teaching_course):
         Mentor. __init__ (self, name, surname, teaching_course)
+
+    def set_student_grade(self, student, course, grade):
+        if course in student.courses_in_progress:
+            if course in self.teaching_course:
+                student.grades[course] = grade
+            else:
+                print(f'Преподаватель этот курс не должен проверять - {course}')
+        else:
+            print(f'Студент не записан на данный курс - {course}')
 
